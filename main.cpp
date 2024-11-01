63e0bff791552ee4d2cc2e18b1ccd560609cee1d
DAM92578 <damuller@fi.uba.ar>
2024-11-01 13:28:32 +0000

    primer commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 //=====[Libraries]=======================================================/*======
 
-/*#include "smart_home_system.h"*/
-//#include "user_interface.h"
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 #include "display.h"
@@ -10,14 +9,11 @@
 
 int main()
 {
-    /*smartHomeSystemInit();*/
-   // while (true) {
-displayInit( DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER );
+
+    displayInit( DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER );
     displayCharPositionWrite ( 0,0);
     displayStringWrite("Taller de Sist. ");
     displayCharPositionWrite ( 3,1);
     displayStringWrite("Embebidos");
 
-       /* smartHomeSystemUpdate();*/
-   // }
 }
